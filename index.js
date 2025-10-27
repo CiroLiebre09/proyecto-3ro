8de53f994394e6074d2250004ce2c90ed79b1115
Ciro Raffaele <you@example.com>
2025-10-27 12:07:39 -0300

    Registro e inicio de sesión terminados

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 import fs from "fs";
 import { subscribeGETEvent, subscribePOSTEvent, realTimeEvent, startServer } from "soquetic";
+let respuesta;
 
 function registrarse(usuario, contraseña)
 {
  let ok;   
 let listaDeUsuarios = JSON.parse(fs.readFileSync("usuarios.json", "utf8"));
-
-
 
 for (let i = 0; i <= listaDeUsuarios.length; i++) 
 {
@@ -16,7 +15,8 @@ if (listaDeUsuarios[i])
  if(usuario.user === listaDeUsuarios[i].user)
 {
  i = listaDeUsuarios.length + 1;
- return console.log("Ese usuario o mail ya está en uso");
+ ok = false;
+ return "Ese usuario o mail ya está en uso";
 }
 }
 if (i === listaDeUsuarios.length)
@@ -24,24 +24,18 @@ if (i === listaDeUsuarios.length)
  listaDeUsuarios.push(usuario, contraseña);
  fs.writeFileSync("usuarios.json", JSON.stringify(listaDeUsuarios, null, 2));
  i = listaDeUsuarios.length + 1;
- return console.log("hecho");
+ ok = true;
+ return "hecho";
 }
 }
-if(ok)
-{
-   
-}
-else{
- 
 }
 
-}
+
+
 function login(usuario, contraseña)
 {
  let ok;   
 let listaDeUsuarios = JSON.parse(fs.readFileSync("usuarios.json", "utf8"));
-
-
 
 for (let i = 0; i <= listaDeUsuarios.length; i++) 
 {
@@ -51,45 +45,20 @@ if (listaDeUsuarios[i])
  if(usuario.user === listaDeUsuarios[i].user && usuario.contraseña === listaDeUsuarios[i].contraseña)
 {
  i = listaDeUsuarios.length + 1;
- return console.log("ah iniciado sesión correctamente");
+ ok = true;
+ return "ah iniciado sesión correctamente";
 }
 }
 if (i === listaDeUsuarios.length)
 {
  i = listaDeUsuarios.length + 1;
- return console.log("El usuario/contraseña es incorrecto");
+ ok = false;
+ return "El usuario/contraseña es incorrecto";
 }
 }
-if(ok)
-{
-   
-}
-else{
- 
-}
-
-}
-
-
-function Login(usuario, mail)
-{
-   let ListaDeUsuarios = fs.readFileSync("usuarios.json", "utf-8");
-    let parsearUsuarios = JSON.parse(leerListaDeUsuarios)
-for (let i = 0; i < ListaDeUsuarios.length; i++) 
-{
- if(usuario === ListaDeUsuarios[i])
-{
-console.log("ya existe");
-}
-if (i === ListaDeUsuarios.length)
-{
-console.log("hecho");
-}
-}
-
 }
 
 subscribePOSTEvent("registro", registrarse);
-
+subscribePOSTEvent("inicioSesion", login);
 
 startServer(3000, true);
--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 let usuario = document.getElementById ("usuario")
 let contraseña = document.getElementById ("contraseña")
-let boton = document.getElementById ("boton")
+let botonRegistrarse = document.getElementById ("botonRegistro")
+let botonIniciarSesion = document.getElementById ("botonLogin")
 
 connect2Server(3000); 
 
-boton.addEventListener("click", function() {
+botonRegistrarse.addEventListener("click", function() {
     let contraseñafinal = contraseña.value
     let usuariofinal = usuario.value
     console.log("front");
@@ -12,6 +13,19 @@ boton.addEventListener("click", function() {
 
 
 postEvent("registro", {user: usuariofinal,contraseña: contraseñafinal}, function(respuesta) {
+console.log("Respuesta del backend:", respuesta);
+alert(respuesta);
+});
+});
+
+botonIniciarSesion.addEventListener("click", function() {
+    let contraseñafinal = contraseña.value
+    let usuariofinal = usuario.value
+    console.log("front");
+    
+
+
+postEvent("inicioSesion", {user: usuariofinal,contraseña: contraseñafinal}, function(respuesta) {
 console.log("Respuesta del backend:", respuesta);
 alert(respuesta);
 });
